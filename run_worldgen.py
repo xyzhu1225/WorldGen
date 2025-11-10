b009104b6e512a10404202c58929b4b6459430a3
xyzhu1225 <xyzhu1225@foxmail.com>
2025-11-09 21:17:45 -0500

    update the newest

--- run_worldgen.py
+++ run_worldgen.py
@@ -6,9 +6,9 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_path', type=str, default="./tmp/tmp", help='Path to save the output mesh')
+    parser.add_argument('--output_path', type=str, default="./tmp/tmp.ply", help='Path to save the output mesh')
     parser.add_argument('--prompt', type=str, default="a warm living room with a fireplace and large windows", help='Text prompt to generate the 3D world')
-    parser.add_argument('--output_type', type=str, choices=['ply', 'glb', 'obj'], default="ply", help='Type of output to save')
+    # parser.add_argument('--output_type', type=str, choices=['ply', 'glb', 'obj'], default="ply", help='Type of output to save')
     parser.add_argument('--device', type=str, default="cuda", help='Device to run the model on')
     args = parser.parse_args()
     
@@ -16,4 +16,4 @@ if __name__ == "__main__":
 
     worldgen = WorldGen(mode="t2s", device=device, low_vram=True) # Set low_vram to True if your GPU VRAM is less than 24GB.
     mesh = worldgen.generate_world(args.prompt, return_mesh=True)
-    o3d.io.write_triangle_mesh(f"{args.output_path}.{args.output_type}", mesh)
+    o3d.io.write_triangle_mesh(f"{args.output_path}", mesh)
